23e9509a12c3e7a18dd29f8cfb511f0e46d70faf
Vishvas Patel <pvishvas7@gmail.com>
2023-02-20 18:13:59 +0530

    navbar added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import logo from '../../assests/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className='navbar'>
+            <Link to='/' className='nav-item nav-btn'>
+                <img src={logo} alt='logo' />
+            </Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
